c0e51abcb2387e1b54000864e9026acdfc6ea398
Parsa <parsa.majdolhosseini@georgebrown.ca>
2023-10-01 22:06:52 -0400

    Lab4 added

---
+++ WEEK4/index.js
@@ -0,0 +1,23 @@
+var express=require("express")
+SERVER_PORT=8089
+var app = express()
+
+app.get("/hello", (req, res)=>{
+    res.send("Hello Express JS")
+})
+
+app.get("/user", (req, res)=>{
+    res.send(req.query)
+})
+
+app.post("/user/:firstname/:lastname", (req, res)=>{
+    let name={
+        firstname:req.params.firstname,
+        lastname:req.params.lastname
+    }
+    res.send(name)
+})
+
+app.listen(SERVER_PORT, ()=>{
+    console.log("Server is listening at http://localhost:8089/")
+})
